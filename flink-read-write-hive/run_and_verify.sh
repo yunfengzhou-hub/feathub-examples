a01ef9232dfdcc0d8c2a30e36c0a4d28508257a0
yunfengzhou-hub <yuri.zhouyunfeng@outlook.com>
2023-05-12 10:53:03 +0800

    tmp

--- flink-read-write-hive/run_and_verify.sh
+++ flink-read-write-hive/run_and_verify.sh
@@ -20,6 +20,8 @@ cd "$(dirname "$0")"
 PROJECT_DIR=$(cd "$(pwd)/.."; pwd)
 source "${PROJECT_DIR}"/tools/utils.sh
 
+wget https://repo.maven.apache.org/maven2/org/apache/flink/flink-sql-connector-hive-3.1.2_2.12/1.16.1/flink-sql-connector-hive-3.1.2_2.12-1.16.1.jar
+
 chmod 777 data
 docker-compose up -d
 wait_for_port 8081 "Flink Cluster"
